4313e4d073ef6f98392430357b78942b57f06bf3
Andrew Odewahn <andrew@odewahn.com>
2016-01-21 15:55:39 -0500

    added email newsletter signup

--- src/components/features.react.js
+++ src/components/features.react.js
@@ -9,6 +9,10 @@ var Colors = require('material-ui/lib/styles/colors');
 
 const FontIcon = require('material-ui/lib/font-icon');
 const IconButton = require('material-ui/lib/icon-button');
+const TextField = require('material-ui/lib/text-field')
+const RaisedButton = require('material-ui/lib/raised-button')
+const Paper = require('material-ui/lib/paper')
+
 
 var FontAwesome = require('react-fontawesome');
 
@@ -41,7 +45,7 @@ module.exports = React.createClass({
   componentWillMount: function() {
     var newMuiTheme = ThemeManager.modifyRawThemePalette(this.state.muiTheme, {
       primary1Color: Colors.orangeA700,
-      canvasColor: Colors.grey700
+      canvasColor: Colors.grey400
     });
     this.setState({muiTheme: newMuiTheme});
   },
@@ -62,10 +66,9 @@ module.exports = React.createClass({
             <div className="col-sm-3">
               <img className="headshot" src="images/headshot.jpg"/>
             </div>
-            <div className="col-sm-9 bioText">
+            <div className="col-sm-5 bioText">
               <p>
                 Hi!  I'm Andrew Odewahn. I'm the current Entrepreneur in Residence and former CTO at <a href="oreilly.com">O'Reilly Media</a>. I'm into developer education, Jupyter, Docker, Go, Carina, and generally lowering the barriers to entry on technology.
-
               </p>
               <div className="social">
                 <a href="https://github.com/odewahn">
@@ -78,6 +81,15 @@ module.exports = React.createClass({
                   <FontAwesome className='socialIcon' name='envelope' size='2x' />
                 </a>
               </div>
+            </div>
+            <div className="col-sm-4">
+              <Paper style={{padding: "20px"}} zDepth={2}>
+                <form action="//odewahn.us12.list-manage.com/subscribe/post?u=b40a78475b5fe9a41af979f72&amp;id=4390e108ac" method="post" id="mc-embedded-subscribe-form">
+                  <h3 style={{color: "white"}}>Sign up for my newsletter to get a free copy of my Docker and DevOps guides:</h3>
+                  <TextField hintText="e-mail" name="EMAIL"/>
+                  <RaisedButton type="submit" secondary={true} value="Subscribe" label="Subscribe" />
+                </form>
+              </Paper>
             </div>
         </div>
         <div className="row">
